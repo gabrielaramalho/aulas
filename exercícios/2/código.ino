d16c3fc24ba9b311f8dc403e44e9f6a2cb0d50f0
Gabriela Ramalho <81374304+gabrielaramalho@users.noreply.github.com>
2022-03-19 18:26:15 -0300

    Update código.ino

--- exercícios/2/código.ino
+++ exercícios/2/código.ino
@@ -1,1 +1,14 @@
-
+void setup()
+{
+  pinMode(2, OUTPUT);//led verde
+  pinMode(3, OUTPUT);//led vermelho
+}
+void loop()
+{
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000); 
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  delay(1000);
+}
