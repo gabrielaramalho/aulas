dfe0b801500019af39345ed29fc4369f6f4bea35
Gabriela Ramalho <81374304+gabrielaramalho@users.noreply.github.com>
2022-03-21 14:38:52 -0300

    Update código.ino

--- exercícios/5/código.ino
+++ exercícios/5/código.ino
@@ -1,1 +1,26 @@
-
+int LED = 2; 
+int BOTAO = 3; 
+int ESTADO_BOTAO = 0; 
+int VAR = 0;
+int VAR2 = 0;
+void setup()
+{
+ pinMode(LED, OUTPUT); 
+ pinMode(BOTAO, INPUT); 
+}
+void loop()
+{
+ VAR = digitalRead(BOTAO); 
+ if (VAR == 1 && VAR2 == 0) //Se botão estiver apertado (HIGH)
+ {
+ ESTADO_BOTAO = 1 - ESTADO_BOTAO;
+   delay(20);
+ }
+ VAR2=VAR;
+  if(ESTADO_BOTAO == 0){
+ digitalWrite(LED, 1);//Acende o led 
+ }
+ else //se estiver pressionado
+ {
+ digitalWrite(LED, 0); //Apaga o led 
+ }
